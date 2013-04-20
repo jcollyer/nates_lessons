114d62332fcbef6a095c83976c7b743db04f8edf
Jeremy Collyer <collyerdesign@gmail.com>
2013-04-20 13:40:37 -0700

    Add fixtures and update views to use model.

--- javascripts/app.coffee
+++ javascripts/app.coffee
@@ -1,14 +1,29 @@
 window.App = Ember.Application.create()
 
+App.Store = DS.Store.extend
+  revision: 12
+  adapter: 'DS.FixtureAdapter'
+
 App.Router.map ->
   @resource "lessons", ->
     @resource "lesson", { path: ":lesson_id"}
 
 
-# //
-# King David's humanity
+App.Lesson = DS.Model.extend
+  title: DS.attr('string')
+  summary: DS.attr('string')
+  imageUrl: DS.attr('string')
+  createdAt: DS.attr('date')
 
-# A studie in psalms of King David seeking God
+Ember.Handlebars.registerBoundHelper 'date', (date) ->
+  moment(date).fromNow()
 
-# 3 months ago
-
+App.Lesson.FIXTURES = [
+  {
+    id: 1,
+    title: "Crying out to God"
+    summary: "A studie in psalms of King David seeking God."
+    imageUrl: "/images/crying_out_to_god.jpg"
+    createdAt: new Date('12-22-2012')
+  }
+]
--- javascripts/app.js
+++ javascripts/app.js
@@ -3,6 +3,11 @@
 
   window.App = Ember.Application.create();
 
+  App.Store = DS.Store.extend({
+    revision: 12,
+    adapter: 'DS.FixtureAdapter'
+  });
+
   App.Router.map(function() {
     return this.resource("lessons", function() {
       return this.resource("lesson", {
@@ -10,5 +15,26 @@
       });
     });
   });
+
+  App.Lesson = DS.Model.extend({
+    title: DS.attr('string'),
+    summary: DS.attr('string'),
+    imageUrl: DS.attr('string'),
+    createdAt: DS.attr('date')
+  });
+
+  Ember.Handlebars.registerBoundHelper('date', function(date) {
+    return moment(date).fromNow();
+  });
+
+  App.Lesson.FIXTURES = [
+    {
+      id: 1,
+      title: "Crying out to God",
+      summary: "A studie in psalms of King David seeking God.",
+      imageUrl: "/images/crying_out_to_god.jpg",
+      createdAt: new Date('12-22-2012')
+    }
+  ];
 
 }).call(this);
