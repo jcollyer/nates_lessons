1bbd91b665bd40427a876c0e9645399346778c5c
Jeremy Collyer <collyerdesign@gmail.com>
2013-04-26 07:58:08 -0700

    style page quote

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
 
-    $("#jquery_jplayer_1").jPlayer({
-        ready: function(event) {
-            $(this).jPlayer("setMedia", {
-                mp3: "http://jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
-                oga: "http://jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg"
-            });
-        },
-        swfPath: "http://jplayer.org/latest/js",
-        supplied: "mp3, oga"
-    });
+//     $("#jquery_jplayer_1").jPlayer({
+//         ready: function(event) {
+//             $(this).jPlayer("setMedia", {
+//                 mp3: "http://jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
+//                 oga: "http://jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg"
+//             });
+//         },
+//         swfPath: "http://jplayer.org/latest/js",
+//         supplied: "mp3, oga"
+//     });
+    var play = $('#art').prop('src');
+    console.log(play);
+
 });                                      
